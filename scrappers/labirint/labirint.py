f29b1135e855e4f639e983ae5ebb15a42e7faa7d
baradys <bym97m@mail.ru>
2022-11-11 11:13:04 +0300

    labirint's scrapper changes

--- scrappers/labirint/labirint.py
+++ scrappers/labirint/labirint.py
@@ -1,9 +1,30 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import datetime
+import csv
+import json
 
 
 def get_data():
+    current_time = datetime.datetime.now().strftime('%m-%d-%Y')
+    with open(f'data/{current_time}_labirint.csv', 'w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file, delimiter=';')
+
+        writer.writerow(
+            [
+                'Название книги',
+                'Автор',
+                'Издательство',
+                'Цена без скидки',
+                'Цена со скидкой',
+                'Процент скидки',
+                'Наличие на складе'
+            ]
+        )
+
     ua = UserAgent()
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
@@ -14,7 +35,8 @@ def get_data():
     response = requests.get(url=url, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
     pages_count = int(soup.find('div', class_='pagination-numbers').find_all('a')[-1].text.strip())
-    for page in range(2, 3):
+    books_data = []
+    for page in range(1,pages_count + 1):
         url = f'https://www.labirint.ru/genres/2498/?display=table&available=1&page={page}'
         response = requests.get(url=url, headers=headers)
         soup = BeautifulSoup(response.text, 'lxml')
@@ -47,10 +69,46 @@ def get_data():
                 discount = f'{round(((old_price - new_price) / old_price) * 100, 2)} %'
             except TypeError:
                 discount = 'Скидки нет'
+            try:
+                availability = book_data[-1].find(class_='mt3 rang-available').text.replace(' ', '').strip()
+            except AttributeError:
+                availability = 'Нет данных'
+            books_data.append(
+                {
+                    'book_title': book_title,
+                    'book_author': book_author,
+                    'book_publisher': book_publisher,
+                    'old_price': f'{old_price}₽' if type(old_price) is int else old_price,
+                    'new_price': f'{new_price}₽' if type(new_price) is int else new_price,
+                    'discount': discount,
+                    'availability': availability,
+                }
+            )
+            with open(f'data/{current_time}_labirint.csv', 'a', newline='', encoding='utf-8-sig') as file:
+                writer = csv.writer(file, delimiter=';')
+
+                writer.writerow(
+                    [
+                        book_title,
+                        book_author,
+                        book_publisher,
+                        f'{old_price}₽' if type(old_price) is int else old_price,
+                        f'{new_price}₽' if type(new_price) is int else new_price,
+                        discount,
+                        availability
+                    ]
+                )
+        print(f'Обработано {page}/{pages_count} страниц')
+
+    with open(f'data/{current_time}-labirint.json', 'w', encoding='utf-8') as file:
+        json.dump(books_data, file, indent=4, ensure_ascii=False)
 
 
 def main():
+    start_time = time.time()
     get_data()
+    diff_time = time.time() - start_time
+    print(f'Затраченное время на работу скрипта - {diff_time}')
 
 
 if __name__ == '__main__':
